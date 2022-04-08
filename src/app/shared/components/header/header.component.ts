dccdbfc72f1d81a11f36c708350d5310163f0139
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-08 19:19:52 +0200

    Exercise 4: Changed header component to have inline template, therefore deleting header.component.html.
    Also leave empty HeaderComponent class at header.component.ts.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `
+    <mat-toolbar color="primary">
+      <span>My Store</span>
+    </mat-toolbar>
+  `,
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
