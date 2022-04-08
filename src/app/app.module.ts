3d3b9ebbfa0b5594aeca1040d825aa75c80418eb
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-08 19:44:50 +0200

    Excercise 5:
    Added new component manually, dominicode.component under pages/dominicode.
    dominicode.component was added to module.ts and added at route localhost/dominicode.
    
    Also added a redirection to routes that are not defined.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DominicodeComponent
   ],
   imports: [
     BrowserModule,
