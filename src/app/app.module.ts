789a2674d2e061ca7a1a9e5ae818fb72b8fd6eab
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-08 19:58:53 +0200

    Excercise 6:
    Added a new module, wich has his own route file, products (under pages/products).
    The creation through ng command automatically set up the route routing.module.ts.
    
    Also deleted everything related to dominicode component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DominicodeComponent
   ],
   imports: [
     BrowserModule,
