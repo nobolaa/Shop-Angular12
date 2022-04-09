34a562cfc998ac8d96dd3d6b85dfda256ee0d400
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-09 13:44:02 +0200

    Exercise 8:
    Added module http at root module.
    Created a product service under products folder.
    Trough it we called a HTTP GET method on products db,
    we saved the results at an observable object of array
    products (type of interface product).
    
    Finally, we called the service at product component.ts to show
    the products via console.log.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MaterialModule } from './material.module';
     AppRoutingModule,
     FormsModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productSvc: ProductsService) { }
 
   ngOnInit(): void {
+    this.productSvc.getProducts()
+      .pipe(
+          tap( res => console.log(res))
+      )
+      .subscribe();
   }
 
 }
