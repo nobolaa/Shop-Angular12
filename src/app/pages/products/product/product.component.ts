6893ae49fe50a3fdc3b18008b08e15b4e6568c9b
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-12 19:12:45 +0200

    Exercise 15:
    Added a checkout module with the corresponding routes, imports from
    material module and changeDetection option at products module.
    
    On the checkout module we added a form requesting some info to perform
    the operation.

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
   @Input() product!: Product;
